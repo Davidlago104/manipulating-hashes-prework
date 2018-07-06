eb6c468809c847cb985bfb0e887f092f112a21f9
Davidlago104 <davidlago104@gmail.com>
2018-07-06 20:53:11 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-[:favorite_icecream_flavors].delete_if("strawberry")
+[:favorite_icecream_flavors].delete("strawberry")
 
 
   #remember to return your newly altered contacts hash!
